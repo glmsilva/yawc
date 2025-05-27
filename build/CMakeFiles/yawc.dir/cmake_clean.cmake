34fc9748bf8926fae7b15d4c4be621d69b63a6bb
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-05-26 22:51:48 -0300

    first commit :tada:

---
+++ build/CMakeFiles/yawc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yawc.dir/src/main.c.o"
+  "CMakeFiles/yawc.dir/src/main.c.o.d"
+  "yawc"
+  "yawc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yawc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/yawc.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for yawc.
+# This may be replaced when dependencies are built.
