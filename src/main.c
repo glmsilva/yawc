05c13929fe9994ec710ad4d72b8a41e942d77f07
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-05-27 23:56:46 -0300

    add -c option to count file bytes size

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h>
+#include <limits.h>
 
-int main(void) {
-    printf("Hello, Yawc");
+int main(int argc, char* argv[]) {
+    if (argc >= 2) {
+        if (strcmp(argv[1], "-c") == 0){
+            char *op = argv[1];
+            char *path = argv[2];
+            
+            FILE *file = fopen(path, "r");
+
+            fseek(file, 0, SEEK_END);
+
+            long fileSize = ftell(file);
+            if (fileSize == -1L) {
+                perror("ftell failed");
+            }
+            else {
+                printf("%ld %s\n", fileSize, path);
+            }
+        }
+    }
+    
     return 0;
 }
