2391bcf9d50d7c063dfb8f5f64c8103df38c04bc
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-05-29 00:07:42 -0300

    add -l option to read number of lines of a given file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include <string.h>
 #include <limits.h>
 
@@ -19,6 +20,37 @@ int main(int argc, char* argv[]) {
             else {
                 printf("%ld %s\n", fileSize, path);
             }
+        } else if (strcmp(argv[1], "-l") == 0) {
+            char *op = argv[1];
+            char *path = argv[2];
+
+            FILE *file = fopen(path, "r");
+
+            char *line_buf = NULL;
+            size_t line_buf_size = 0;
+
+            int line_count = 0;
+            ssize_t line_size;
+
+            if (!file) {
+                    fprintf(stderr, "Error opening file %s\n", path);
+                    return 0;
+            }
+
+            line_size = getline(&line_buf, &line_buf_size, file);
+
+            while (line_size >= 0) {
+                line_count++;
+
+                line_size = getline(&line_buf, &line_buf_size, file);
+            }
+
+            free(line_buf);
+            line_buf = NULL;
+
+            fclose(file);
+
+            printf("%d %s\n", line_count, path);
         }
     }
     
