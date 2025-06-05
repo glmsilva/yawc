b3533ee8cec144937240b53f2befbe2c076f3ce0
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-06-04 21:01:35 -0300

    add -w option to count words

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <cxxopts.hpp>
 #include <fstream>
 #include <iostream>
@@ -6,9 +7,10 @@ int main(int argc, char *argv[])
 {
     cxxopts::Options options("Yawc", "Yet Another Word Counter");
 
-    options.add_options()
-    ("c, bytes", "Count bytesize", cxxopts::value<std::string>())
-    ("l, lines", "Count lines", cxxopts::value<std::string>());
+    options.add_options()("c, bytes", "Count bytesize",
+                          cxxopts::value<std::string>())(
+        "l, lines", "Count lines", cxxopts::value<std::string>())(
+        "w, words", "Count words", cxxopts::value<std::string>());
 
     auto result = options.parse(argc, argv);
 
@@ -58,6 +60,42 @@ int main(int argc, char *argv[])
         }
 
         std::cout << line_counter << '\n';
+    }
+    if (result.count("w"))
+    {
+        std::string path;
+
+        path = result["w"].as<std::string>();
+
+        std::ifstream file(path);
+
+        if (!file)
+        {
+            std::cerr << "Error handling file" << '\n';
+        }
+
+        int word_counter{};
+
+        for (std::string line; std::getline(file, line);)
+        {
+            bool in_word = false;
+
+            for (char ch : line)
+            {
+
+                if (isspace(ch))
+                {
+                    in_word = false;
+                }
+                else if (!in_word)
+                {
+                    word_counter++;
+                    in_word = true;
+                }
+            }
+        }
+
+        std::cout << word_counter << '\n';
     }
     return 0;
 }
